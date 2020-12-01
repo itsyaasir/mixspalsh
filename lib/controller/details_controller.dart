469e6e9556888f1cfedbc3f650c8b2b92afa7f80
Yasir <goodwonder5@gmail.com>
2020-12-01 22:07:54 +0300

    overhaul of some pages

---
+++ lib/controller/details_controller.dart
@@ -0,0 +1,6 @@
+import 'package:get/get.dart';
+import 'package:meta/meta.dart';
+
+class DetailsController extends GetxController {
+  
+}
--- lib/screens/details_page.dart
+++ lib/screens/details_page.dart
@@ -10,11 +10,48 @@ class DetailsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    ImageSaver imageSaver = ImageSaver();
+    // ImageSaver imageSaver = ImageSaver();
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Container(
-        color: Colors.red,
+      body: Stack(
+        children: [
+          Image.network(
+            "${Get.arguments[1]}",
+            fit: BoxFit.cover,
+            height: Get.height,
+            width: Get.width,
+          ),
+          Positioned(
+            bottom: 0,
+            child: Padding(
+              padding: const EdgeInsets.all(32.0),
+              child: Row(
+                children: [
+                  IconButton(
+                    icon: Icon(Icons.download_outlined),
+                    iconSize: 30,
+                    color: Colors.white70,
+                    onPressed: () {},
+                  ),
+                  SizedBox(width: 10),
+                  IconButton(
+                    icon: Icon(Icons.share_outlined),
+                    iconSize: 30,
+                    color: Colors.white70,
+                    onPressed: () {},
+                  ),
+                  SizedBox(width: 10),
+                  IconButton(
+                    icon: Icon(Icons.info_outline_rounded),
+                    iconSize: 30,
+                    color: Colors.white70,
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
       ),
     );
   }

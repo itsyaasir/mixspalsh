403ef584018f28720d82b7357387d6832be0b970
Yasir <goodwonder5@gmail.com>
2020-12-04 00:32:04 +0300

    add progress indicator when loading the images

--- lib/app/screens/details/details_page.dart
+++ lib/app/screens/details/details_page.dart
@@ -11,14 +11,20 @@ class DetailsPage extends GetView<DetailsController> {
       body: Stack(
         children: [
           controller.obx(
-            // Use this for better loading and error showing.
-            (state) => ImageWidget(
-              imageUrl: "${state.urls.regular}",
-              hashBlur: "${state.blurHash}",
-              height: Get.height,
-              width: Get.width,
-            ),
-          ),
+              // Use this for better loading and error showing.
+
+              (state) => ImageWidget(
+                    imageUrl: "${state.urls.regular}",
+                    hashBlur: "${state.blurHash}",
+                    height: Get.height,
+                    width: Get.width,
+                  ),
+              onLoading: Center(
+                child: CircularProgressIndicator(
+                  backgroundColor: Colors.white,
+                  valueColor: new AlwaysStoppedAnimation(Colors.black),
+                ),
+              )),
           Positioned(
             bottom: 0,
             child: Padding(
